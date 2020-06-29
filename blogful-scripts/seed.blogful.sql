32b6ed1ab9ea4955be68539900d34ed12b20b77a
Aaron <amsegal93@gmail.com>
2020-06-29 12:18:05 -0400

    Pushing final practice material

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO blogful_articles (title, date_published, content)
+
+VALUES
+    ('Top 10 Starter Pokemon', '2019-05-12 12:00:05', 'The top 10 starter pokemon are just totodile 10 times.'),
+    ('Why Eating Humans is a good idea', '1034-11-11 12:45:54', 'It would both provide a new food source as well as lower humanities food consumption'),
+    ('Coffee, that addictive substance we all dont need', '2020-03-11 15:23:54', 'Aww who are we kidding, we deffinetly need that stuff to function')
